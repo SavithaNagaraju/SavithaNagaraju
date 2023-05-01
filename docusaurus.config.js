2c3768fb0392fc5ea2901f0bc3a652cfda560102
prave <praveenneuron@gmail.com>
2023-05-01 10:12:43 +0200

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/SavithaNagaraju/SavithaNagaraju.github.io/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/SavithaNagaraju/SavithaNagaraju/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/SavithaNagaraju/SavithaNagaraju.github.io/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/SavithaNagaraju/SavithaNagaraju/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
